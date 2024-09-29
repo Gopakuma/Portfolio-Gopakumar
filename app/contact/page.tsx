c0c4db2c5b7e158b07f1e566159316d21b2da978
Gopakuma <gopakumarg893@gmail.com>
2024-09-29 13:22:19 +0530

    Contact commented

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,30 +1,30 @@
-import nodemailer from "nodemailer"
+// import nodemailer from "nodemailer"
 
-type TEmailBody = {
-    email: string;
-    subject: string;
-    message: string;
-};
+// type TEmailBody = {
+//     email: string;
+//     subject: string;
+//     message: string;
+// };
 
 
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-        user: "gopakumarg893@gmail.com",
-        pass: "rayq stcm nzlk avds",
-    },
-});
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export default async function POST(req: any) {
-    const data: TEmailBody = await req.searchParams;
-    const info = await transporter.sendMail({
-        from: data.email, // sender address
-        to: "gopakumarg893@gmail.com", // list of receivers
-        subject: data.subject, // Subject line
-        html: `<b>${data.message}</b>`, // html body
-    });
+// const transporter = nodemailer.createTransport({
+//     host: "smtp.gmail.com",
+//     port: 587,
+//     secure: false,
+//     auth: {
+//         user: "gopakumarg893@gmail.com",
+//         pass: "rayq stcm nzlk avds",
+//     },
+// });
+// /* eslint-disable @typescript-eslint/no-explicit-any */
+// export default async function POST(req: any) {
+//     const data: TEmailBody = await req.searchParams;
+//     const info = await transporter.sendMail({
+//         from: data.email, // sender address
+//         to: "gopakumarg893@gmail.com", // list of receivers
+//         subject: data.subject, // Subject line
+//         html: `<b>${data.message}</b>`, // html body
+//     });
 
-    console.log("Message sent: %s", info.messageId);
-}
+//     console.log("Message sent: %s", info.messageId);
+// }
